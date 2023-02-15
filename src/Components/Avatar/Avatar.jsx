bf30ce49402f6196a7e363edb52ab85ce0c6c34f
adewale-samson <mascot4sure@gmail.com>
2023-02-15 23:16:18 +0100

    latest avatar update

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -5,7 +5,7 @@ import { Box } from '@mui/material'
 
 export default function TotalAvatars() {
   return (
-    <Box sx={{ margin: {xs:'25px auto 100px auto',sm:'25px auto 100px auto',md:'25px auto 334px auto',lg:'25px auto 334px auto'}, '& .css-6blv01-MuiAvatarGroup-root': {width:'200px', margin:'0 auto'}}}>
+    <Box sx={{  display:'flex', justifyContent:'center', margin: {xs:'25px auto 100px auto',sm:'25px auto 100px auto',md:'25px auto 334px auto',lg:'25px auto 334px auto'}, '& .MuiAvatarGroup-root': {width:'200px', margin:'0 auto', border:'none'}}}>
         <AvatarGroup total={61} sx={{fontSize: '16px', '& .css-sxh3gq-MuiAvatar-root-MuiAvatarGroup-avatar':{border: 'none !important', marginLeft: '20px', background: 'inherit', borderRadius: '0', fontFamily: 'inherit'}}}>
         <Avatar alt="Gemy Sharp" src="/static/images/avatar/1.jpg" sx={{background: '#175CE1', border:'none !important', width: '32px', height: '32px', padding: '2px'}}/>
         <Avatar alt="Oravis Howard" src="/static/images/avatar/2.jpg" sx={{background: '#A75FD2', border:'none !important', width: '32px', height: '32px', padding: '2px'}}/>
@@ -15,3 +15,21 @@ export default function TotalAvatars() {
     </Box>
   );
 }
+
+// import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import AvatarGroup from '@mui/material/AvatarGroup';
+// import { Box } from '@mui/material'
+
+// export default function TotalAvatars() {
+//   return (
+//     <Box sx={{  display:'flex', justifyContent:'center', margin: {xs:'25px auto 100px auto',sm:'25px auto 100px auto',md:'25px auto 334px auto',lg:'25px auto 334px auto'}, '& .MuiAvatarGroup-root': {width:'200px', margin:'0 auto'}}}>
+//         <AvatarGroup total={61} sx={{fontSize: '16px', '& .css-sxh3gq-MuiAvatar-root-MuiAvatarGroup-avatar':{border: 'none !important', marginLeft: '20px', background: 'inherit', borderRadius: '0', fontFamily: 'inherit'}}}>
+//         <Avatar alt="Gemy Sharp" src="/static/images/avatar/1.jpg" sx={{background: '#175CE1', border:'none !important', width: '32px', height: '32px', padding: '2px'}}/>
+//         <Avatar alt="Oravis Howard" src="/static/images/avatar/2.jpg" sx={{background: '#A75FD2', border:'none !important', width: '32px', height: '32px', padding: '2px'}}/>
+//         <Avatar alt="Ugnes Walker" src="/static/images/avatar/4.jpg" sx={{background: '#175CE1', border:'none !important', width: '32px', height: '32px', padding: '2px'}}/>
+//         <Avatar alt="Mrevor Henderson" src="/static/images/avatar/5.jpg" sx={{background: '#A75FD2', border:'none !important', width: '32px', height: '32px', padding: '2px'}}/>
+//         </AvatarGroup>
+//     </Box>
+//   );
+// }
